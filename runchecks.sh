0e8d5e08657e6e3e61377566d8764d0457c52272
Anthony Shaw <anthony.p.shaw@gmail.com>
2020-12-03 12:42:27 +1100

    Update runchecks.sh

--- runchecks.sh
+++ runchecks.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Performing checkup:"
-cppcheck $3 --language=c++ --enable=$2 --template="::{severity} file={file},line={line},col={column}::{message}" $1
+cppcheck $3 --language=c++ --enable="$2" --template="::{severity} file={file},line={line},col={column}::{message}" $1

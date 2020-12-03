b86bf2b7238e3e1ef4599ee0b70372ec674e1c35
Anthony Shaw <anthony.p.shaw@gmail.com>
2020-12-03 11:40:31 +1100

    Update runchecks.sh

--- runchecks.sh
+++ runchecks.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cd $1
 echo "Performing checkup:"
 clang-tidy --version
 clang-tidy *.c *.h *.cpp *.hpp *.C *.cc *.CPP *.c++ *.cp *.cxx -checks=boost-*,bugprone-*,performance-*,readability-*,portability-*,modernize-*,clang-analyzer-cplusplus-*,clang-analyzer-*,cppcoreguidelines-* > clang-tidy-report.txt

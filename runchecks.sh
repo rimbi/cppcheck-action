613351d1a8dffbbedea2706f73201f1c895987e8
Anthony Shaw <anthony.p.shaw@gmail.com>
2020-12-03 11:29:26 +1100

    Update runchecks.sh

--- runchecks.sh
+++ runchecks.sh
@@ -1,29 +1,6 @@
 #!/bin/bash
 
-if [[ -z "$GITHUB_TOKEN" ]]; then
-	echo "The GITHUB_TOKEN is required."
-	exit 1
-fi
-
-FILES_LINK=`jq -r '.pull_request._links.self.href' "$GITHUB_EVENT_PATH"`/files
-echo "Files = $FILES_LINK"
-
-curl $FILES_LINK > files.json
-FILES_URLS_STRING=`jq -r '.[].raw_url' files.json`
-
-readarray -t URLS <<<"$FILES_URLS_STRING"
-
-echo "File names: $URLS"
-
-mkdir files
-cd files
-for i in "${URLS[@]}"
-do
-   echo "Downloading $i"
-   curl -LOk --remote-name $i 
-done
-
-echo "Files downloaded!"
+cd $1
 echo "Performing checkup:"
 clang-tidy --version
 clang-tidy *.c *.h *.cpp *.hpp *.C *.cc *.CPP *.c++ *.cp *.cxx -checks=boost-*,bugprone-*,performance-*,readability-*,portability-*,modernize-*,clang-analyzer-cplusplus-*,clang-analyzer-*,cppcoreguidelines-* > clang-tidy-report.txt
